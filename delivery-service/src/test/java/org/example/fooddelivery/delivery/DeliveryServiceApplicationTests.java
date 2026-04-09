95448aacff40d3381fd8b855ca21d8d51de0293c
alfreddey <lilscar246@gmail.com>
2026-04-09 10:54:36 +0000

    add delivery-service
    Add Spring Boot microservice for delivery management with Maven wrapper

---
+++ delivery-service/src/test/java/org/example/fooddelivery/delivery/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fooddelivery.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
